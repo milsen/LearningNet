3a554b58af33301af648ddc6f336833c44ced5c3
milsen <milsen@users.noreply.github.com>
2019-10-13 03:01:50 +0200

    Add save functions to Costs-models

--- models/NodeCosts.php
+++ models/NodeCosts.php
@@ -20,12 +20,33 @@ class NodeCosts extends Costs
     }
 
     /**
-     * @return an array of the form
+     * @return array of the form
      * [ { 'weight' => "weight", 'costs' => { "block_id" => "cost" } ]
      */
     static public function costs($courseId) {
         return parent::costs($courseId, function(&$currentCosts, $row) {
             $currentCosts[$row['block_id']] = $row['cost'];
         });
+    }
+
+    /**
+     * Stores cost of a Courseware section according to a given cost function.
+     *
+     * @param string $courseId id of the course
+     * @param string $costFunc name/id of the cost function
+     * @param string $sectionId block id of the Courseware section
+     * @param int $costValue value in [0,100]
+     * @return void
+     */
+    static public function save($courseId, $costFunc, $sectionId, $costValue) {
+        $costRep = self::find([$courseId, $costFunc, $sectionId]);
+        if ($costRep === null) {
+            $costRep = new self;
+            $costRep->seminar_id = $courseId;
+            $costRep->cost_func = $costFunc;
+            $costRep->block_id = $sectionId;
+        }
+        $costRep->cost = $costValue;
+        $costRep->store();
     }
 }
--- models/NodePairCosts.php
+++ models/NodePairCosts.php
@@ -21,7 +21,7 @@ class NodePairCosts extends Costs
     }
 
     /**
-     * @return an array of the form
+     * @return array of the form
      * [ { 'weight' => "weight", 'costs' => { "block_id_from" => { "block_id_to" => "cost" } } ]
      */
     static public function costs($courseId) {
@@ -31,5 +31,32 @@ class NodePairCosts extends Costs
             }
             $currentCosts[$row['block_id_from']][$row['block_id_to']] = $row['cost'];
         });
+    }
+
+    /**
+     * Stores cost of a pair of Courseware sections according to a given cost
+     * function.
+     *
+     * @param string $courseId id of the course
+     * @param string $costFunc name/id of the cost function
+     * @param string $sectionIdFrom block id of the first Courseware section
+     * @param string $sectionIdTo block id of the second Courseware section
+     * @param int $costValue value in [0,100]
+     * @return void
+     */
+    static public function save($courseId, $costFunc,
+        $sectionIdFrom, $sectionIdTo, $costValue) {
+        $costRep = self::find([$courseId, $costFunc,
+            $sectionIdFrom, $sectionIdTo
+        ]);
+        if ($costRep === null) {
+            $costRep = new self;
+            $costRep->seminar_id = $courseId;
+            $costRep->cost_func = $costFunc;
+            $costRep->block_id_from = $sectionIdFrom;
+            $costRep->block_id_to = $sectionIdTo;
+        }
+        $costRep->cost = $costValue;
+        $costRep->store();
     }
 }
